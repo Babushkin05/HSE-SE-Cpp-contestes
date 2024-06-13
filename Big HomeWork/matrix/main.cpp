1efb2b120b436a0b8693ff6d479d9434b5a52e0f
vova babushkin <babushkinvova05@gmail.com>
2024-06-13 23:56:59 +0300

    bdz a

--- Big HomeWork/matrix/main.cpp
+++ Big HomeWork/matrix/main.cpp
@@ -3,5 +3,11 @@
 #include <type_traits>
 
 int main(){
-    
+    Matrix<int,3,2> m{1,2,3,4,5,9};
+    Matrix<int,2,2> a{1,2,3,4};
+    Matrix<int,2,2> b{1,2,3,4};
+    std::cout<<m;
+    std::cout<< (m*=a)<<m;
+    std::cout<<(7*(GetTransposed(a)-b) == 7 * (GetTransposed(b) - a));
+    std::cout<<m.ColumnsNumber()<<' '<<m.RowsNumber();
 }
